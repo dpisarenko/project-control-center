ab886f9012dc4cafcc133a86515f328bd970a403
Dmitri Pisarenko <dp@sw-dev.at>
2010-12-04 23:59:02 +0100

    Added headers to some files

--- release.sh
+++ release.sh
@@ -1,3 +1,11 @@
+# This file is part of Project Control Center (PCC).
+# 
+# PCC (Project Control Center) project is intellectual property of 
+# Dmitri Anatol'evich Pisarenko.
+# 
+# Copyright 2010 Dmitri Anatol'evich Pisarenko
+# All rights reserved
+
 mvn clean
 mvn release:clean
 git commit -am "Automatic commit before mvn release:prepare"
