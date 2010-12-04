ddddeca496e849944caa84b48ab0eba37acca533
Dmitri Pisarenko <dp@sw-dev.at>
2010-12-05 00:35:20 +0100

    Fixed some checkstyle errors.

--- src/main/java/at/silverstrike/pcc/api/embeddedfilereading/EmbeddedFileReaderFactory.java
+++ src/main/java/at/silverstrike/pcc/api/embeddedfilereading/EmbeddedFileReaderFactory.java
@@ -1,22 +1,13 @@
 /**
  * This file is part of Project Control Center (PCC).
  * 
- * Project Control Center (PCC) is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
  * 
- * Project Control Center (PCC) is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with Project Control Center (PCC).  If not, see <http://www.gnu.org/licenses/>.
- *
  * Copyright 2010 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
  **/
-
 package at.silverstrike.pcc.api.embeddedfilereading;
 
 import at.silverstrike.pcc.api.conventions.Factory;
