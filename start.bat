07d4ccdf4b983601f86c0435c0dfc8d209224e26
Dmitri Pisarenko <dp@sw-dev.at>
2011-01-07 01:59:19 +0100

    Initial commit.

---
+++ start.bat
@@ -0,0 +1,11 @@
+﻿REM
+REM This file is part of Project Control Center (PCC).
+REM 
+REM 
+REM PCC (Project Control Center) project is intellectual property of 
+REM Dmitri Anatol'evich Pisarenko.
+REM 
+REM Copyright 2010, 2011 Dmitri Anatol'evich Pisarenko
+REM All rights reserved
+
+mvn jetty:run
