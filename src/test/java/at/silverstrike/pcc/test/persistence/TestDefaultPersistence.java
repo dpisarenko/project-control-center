39b9b6040df08eae1458534789b10aaa1f183378
Dmitri Pisarenko <dp@sw-dev.at>
2011-01-19 16:07:18 +0100

    Added test case for defect #57.

--- src/test/java/at/silverstrike/pcc/test/persistence/TestDefaultPersistence.java
+++ src/test/java/at/silverstrike/pcc/test/persistence/TestDefaultPersistence.java
@@ -12,11 +12,14 @@
 package at.silverstrike.pcc.test.persistence;
 
 import java.util.Date;
+import java.util.List;
 
 import junit.framework.Assert;
 
 import org.junit.Test;
 
+import at.silverstrike.pcc.api.model.ControlProcess;
+import at.silverstrike.pcc.api.model.ProcessState;
 import at.silverstrike.pcc.api.persistence.Persistence;
 import at.silverstrike.pcc.impl.persistence.DefaultPersistence;
 
@@ -52,5 +55,48 @@ public class TestDefaultPersistence {
             Assert.fail(exception.getMessage());
         }
 
+    }
+    @Test
+    public void testDefect57()
+    {
+    	// Get object under test (persistence)
+        final Persistence persistence = new DefaultPersistence();
+        
+        try
+        {
+            persistence.openSession();   
+        }
+        catch (final RuntimeException exception)
+        {
+            Assert.fail(exception.getMessage());
+        }
+        catch (final Exception exception)
+        {
+            Assert.fail(exception.getMessage());
+        }
+    	
+    	// Clear database
+        persistence.clearDatabase();
+        
+    	// Add one task
+        final Long taskId = persistence.createTask("Bug #57");
+        
+    	// Verify it's present in the list of not attained tasks
+        final List<ControlProcess> processList1 = persistence.getAllNotDeletedTasks();
+        
+        Assert.assertEquals(1, processList1.size());
+        
+    	// Mark the task as attained
+    	final ControlProcess process = persistence.getTask(taskId);
+    	Assert.assertNotNull(process);
+        
+    	process.setState(ProcessState.ATTAINED);
+    	persistence.updateTask(process);
+    	
+    	// Verify it's not present in the list of not attained tasks
+        final List<ControlProcess> processList2 = persistence.getAllNotDeletedTasks();
+        
+        Assert.assertEquals(0, processList2.size());
+    	
     }
 }
