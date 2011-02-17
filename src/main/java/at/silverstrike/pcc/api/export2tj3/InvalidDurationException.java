e72c70b33d0e685d50da86d3cc3e2510ae9f1006
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-17 11:43:10 +0100

    Defect Nr. 62: Added Masha's changes.

--- src/main/java/at/silverstrike/pcc/api/export2tj3/InvalidDurationException.java
+++ src/main/java/at/silverstrike/pcc/api/export2tj3/InvalidDurationException.java
@@ -2,29 +2,31 @@ package at.silverstrike.pcc.api.export2tj3;
 
 import at.silverstrike.pcc.api.conventions.PccException;
 
-public class InvalidDurationException  extends PccException {
-	private static final long serialVersionUID = 1L;
-	
-	private Long taskNumber;
-	private String taskName;
-
-	public Long getTaskNumber() {
-		return taskNumber;
-	}
-	public void setTaskNumber(Long taskNumber) {
-		this.taskNumber = taskNumber;
-	}
-	public String getTaskName() {
-		return taskName;
-	}
-	public void setTaskName(String taskName) {
-		this.taskName = taskName;
-	}
-
-	public InvalidDurationException(final Long aTaskNumber, final String aTaskName)
-	{
-		super("Invalid duration");
-		this.taskNumber = aTaskNumber;
-		this.taskName = aTaskName;
-	}
+/**
+ * This exception is thrown, if TaskJugglerExporter is invoked with
+ * invalid timingresolution. Timingresolution must equal-to-or-greater-than 15 min
+ */
+public class InvalidDurationException extends PccException {
+    private static final long serialVersionUID = 1L;
+    
+    private Long taskNumber;
+    private String taskName;
+    
+    public InvalidDurationException(Long taskNumber, String taskName)
+    {
+        super("Invalid timingresolution. Must be equal-to-or-greater-than 15 min");
+        
+        this.taskNumber = taskNumber;   
+        this.taskName = taskName;
+    }
+    
+    public Long getTaskNumber()
+    {
+    	return taskNumber;
+    }   
+    
+    public String getTaskName()
+    {
+    	return taskName;
+    }
 }
