4edae01d875b0a0a3666bc12d05ad6e4b9edda9c
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-19 17:03:20 +0100

    Fixed some checkstyle errors.

--- src/main/java/at/silverstrike/pcc/api/export2tj3/InvalidDurationException.java
+++ src/main/java/at/silverstrike/pcc/api/export2tj3/InvalidDurationException.java
@@ -1,32 +1,42 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
 package at.silverstrike.pcc.api.export2tj3;
 
 import at.silverstrike.pcc.api.conventions.PccException;
 
 /**
- * This exception is thrown, if TaskJugglerExporter is invoked with
- * invalid timingresolution. Timingresolution must equal-to-or-greater-than 15 min
+ * This exception is thrown, if TaskJugglerExporter is invoked with invalid
+ * timingresolution. Timingresolution must equal-to-or-greater-than 15 min
  */
 public class InvalidDurationException extends PccException {
-    private static final long serialVersionUID = 1L;
-    
-    private Long taskNumber;
-    private String taskName;
-    
-    public InvalidDurationException(Long taskNumber, String taskName)
-    {
-        super("Invalid timingresolution. Must be equal-to-or-greater-than 15 min");
-        
-        this.taskNumber = taskNumber;   
-        this.taskName = taskName;
-    }
-    
-    public Long getTaskNumber()
-    {
-    	return taskNumber;
-    }   
-    
-    public String getTaskName()
-    {
-    	return taskName;
-    }
+	private static final long serialVersionUID = 1L;
+
+	private Long taskNumber;
+	private String taskName;
+
+	public InvalidDurationException(final Long aTaskNumber,
+			final String aTaskName) {
+		super(
+				"Invalid timingresolution. Must be equal-to-or-greater-than 15 min");
+
+		this.taskNumber = aTaskNumber;
+		this.taskName = aTaskName;
+	}
+
+	public final Long getTaskNumber() {
+		return taskNumber;
+	}
+
+	public final String getTaskName() {
+		return taskName;
+	}
 }
