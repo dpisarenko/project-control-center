c452c364802f83e3cce3f349c130b30d61c7dab4
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-19 10:28:11 +0100

    Defect 62: Changed the message window, changed the wording of the error message.

--- src/main/java/at/silverstrike/pcc/impl/schedulingpanel/DefaultSchedulingPanel.java
+++ src/main/java/at/silverstrike/pcc/impl/schedulingpanel/DefaultSchedulingPanel.java
@@ -11,6 +11,8 @@
 
 package at.silverstrike.pcc.impl.schedulingpanel;
 
+import static com.vaadin.ui.Window.Notification.TYPE_ERROR_MESSAGE;
+
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -19,6 +21,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import at.silverstrike.pcc.api.conventions.PccException;
+import at.silverstrike.pcc.api.export2tj3.InvalidDurationException;
 import at.silverstrike.pcc.api.model.Resource;
 import at.silverstrike.pcc.api.persistence.Persistence;
 import at.silverstrike.pcc.api.projectscheduler.ProjectScheduler;
@@ -28,132 +31,154 @@ import at.silverstrike.pcc.api.schedulingpanel.SchedulingPanel;
 import com.google.inject.Injector;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.ProgressIndicator;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Window;
+import com.vaadin.ui.Window.Notification;
 
 import eu.livotov.tpt.i18n.TM;
 
 class DefaultSchedulingPanel extends Panel implements SchedulingPanel {
-    private static final int ONE_MONTH = 1;
+	private static final int ONE_MONTH = 1;
 
-    private static final String NEWLINE = System.getProperty("line.separator");
+	private static final String NEWLINE = System.getProperty("line.separator");
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private transient Injector injector;
-    private final static Logger LOGGER =
-        LoggerFactory.getLogger(DefaultSchedulingPanel.class);
-    private ProgressIndicator progressIndicator;
+	private transient Injector injector;
+	private final static Logger LOGGER = LoggerFactory
+			.getLogger(DefaultSchedulingPanel.class);
+	private ProgressIndicator progressIndicator;
 
-    private TextField loggingTextArea;
-    private transient Persistence persistence;
+	private TextField loggingTextArea;
+	private transient Persistence persistence;
 
-    public DefaultSchedulingPanel() {
-    }
+	public DefaultSchedulingPanel() {
+	}
 
-    @Override
-    public void initGui() {
-        this.setWidth("500px");
-        this.setHeight("100%");
+	@Override
+	public void initGui() {
+		this.setWidth("500px");
+		this.setHeight("100%");
 
-        setCaption(TM.get("schedulingpanel.1-caption"));
+		setCaption(TM.get("schedulingpanel.1-caption"));
 
-        final VerticalLayout layout = new VerticalLayout();
+		final VerticalLayout layout = new VerticalLayout();
 
-        final HorizontalLayout progressAndButtonPanel = new HorizontalLayout();
-        progressIndicator = new ProgressIndicator();
-        progressIndicator.setIndeterminate(true);
-        progressIndicator.setEnabled(false);
+		final HorizontalLayout progressAndButtonPanel = new HorizontalLayout();
+		progressIndicator = new ProgressIndicator();
+		progressIndicator.setIndeterminate(true);
+		progressIndicator.setEnabled(false);
 
-        final Button startButton =
-                new Button(TM.get("schedulingpanel.2-start-button"));
+		final Button startButton = new Button(
+				TM.get("schedulingpanel.2-start-button"));
 
-        startButton.addListener(new ClickListener() {
-            private static final long serialVersionUID = 1L;
+		startButton.addListener(new ClickListener() {
+			private static final long serialVersionUID = 1L;
 
-            @Override
-            public void buttonClick(final ClickEvent event) {
-                startButtonClicked();
+			@Override
+			public void buttonClick(final ClickEvent event) {
+				startButtonClicked();
+			}
+		});
+
+		progressAndButtonPanel.addComponent(progressIndicator);
+		progressAndButtonPanel.addComponent(startButton);
+
+		layout.addComponent(progressAndButtonPanel);
+
+		loggingTextArea = new TextField();
+		loggingTextArea.setRows(20);
+		loggingTextArea.setColumns(20);
+
+		layout.addComponent(loggingTextArea);
+
+		addComponent(layout);
+	}
+
+	@Override
+	public void setInjector(final Injector anInjector) {
+		injector = anInjector;
+
+		if (anInjector != null) {
+			this.persistence = anInjector.getInstance(Persistence.class);
+		}
+	}
+
+	@Override
+	public Panel toPanel() {
+		return this;
+	}
+
+	protected void startButtonClicked() {
+		progressIndicator.setEnabled(true);
+
+		final ProjectSchedulerFactory factory = injector
+				.getInstance(ProjectSchedulerFactory.class);
+		final ProjectScheduler scheduler = factory.create();
+
+		scheduler.getProjectExportInfo().setControlProcessesToExport(
+				this.persistence.getAllNotDeletedTasks());
+
+		final List<Resource> resources = new LinkedList<Resource>();
+		resources.addAll(this.persistence.getAllWorkers());
+
+		scheduler.getProjectExportInfo().setResourcesToExport(resources);
+
+		scheduler.getProjectExportInfo().setProjectName("pcc");
+
+		final Date now = new Date();
+
+		scheduler.getProjectExportInfo().setNow(now);
+		scheduler.getProjectExportInfo().setCopyright("Dmitri Pisarenko");
+		scheduler.getProjectExportInfo().setCurrency("EUR");
+		scheduler.getProjectExportInfo().setSchedulingHorizonMonths(ONE_MONTH);
+
+		appendToLoggingTextArea("0");
+
+		scheduler.setDirectory(System.getProperty("user.dir") + "/");
+		scheduler.setInjector(injector);
+		scheduler.setNow(now);
+
+		try {
+			appendToLoggingTextArea("1");
+			scheduler.run();
+			appendToLoggingTextArea("2");
+			progressIndicator.setEnabled(false);
+		} catch (final InvalidDurationException exception) {
+        	Window subwindow = new Window(TM.get("schedulingpanel.3-start-validation-error-caption"));
+        	
+			subwindow.setModal(true);
+            Label message = new Label(TM.get("schedulingpanel.4-start-validation-error-description", exception.getTaskNumber(), exception.getTaskName()));
+            subwindow.addComponent(message);
+            
+        	if (subwindow.getParent() != null) {
+                // window is already showing
+                getWindow().showNotification(
+                        "Window is already open");
+            } else {
+                // Open the subwindow by adding it to the parent
+                // window
+                getWindow().addWindow(subwindow);
             }
-        });
+			progressIndicator.setEnabled(false);
+		}
 
-        progressAndButtonPanel.addComponent(progressIndicator);
-        progressAndButtonPanel.addComponent(startButton);
+		catch (final PccException exception) {
+			appendToLoggingTextArea("3" + exception.toString());
+			LOGGER.error("", exception);
+			progressIndicator.setEnabled(false);
+			appendToLoggingTextArea("4");
+		}
+	}
 
-        layout.addComponent(progressAndButtonPanel);
-
-        loggingTextArea = new TextField();
-        loggingTextArea.setRows(20);
-        loggingTextArea.setColumns(20);
-
-        layout.addComponent(loggingTextArea);
-
-        addComponent(layout);
-    }
-
-    @Override
-    public void setInjector(final Injector anInjector) {
-        injector = anInjector;
-        
-        if (anInjector != null)
-        {
-            this.persistence = anInjector.getInstance(Persistence.class);
-        }
-    }
-
-    @Override
-    public Panel toPanel() {
-        return this;
-    }
-
-    protected void startButtonClicked() {
-        progressIndicator.setEnabled(true);
-
-        final ProjectSchedulerFactory factory =
-                injector.getInstance(ProjectSchedulerFactory.class);
-        final ProjectScheduler scheduler = factory.create();
-
-        scheduler.getProjectExportInfo().setControlProcessesToExport(this.persistence.getAllNotDeletedTasks());
-        
-        final List<Resource> resources = new LinkedList<Resource>();
-        resources.addAll(this.persistence.getAllWorkers());
-        
-        scheduler.getProjectExportInfo().setResourcesToExport(resources);
-        
-        scheduler.getProjectExportInfo().setProjectName("pcc");
-        
-        final Date now = new Date();
-        
-        scheduler.getProjectExportInfo().setNow(now );
-        scheduler.getProjectExportInfo().setCopyright("Dmitri Pisarenko");
-        scheduler.getProjectExportInfo().setCurrency("EUR");
-        scheduler.getProjectExportInfo().setSchedulingHorizonMonths(ONE_MONTH);
-        
-        appendToLoggingTextArea("0");
-        
-        scheduler.setDirectory(System.getProperty("user.dir") + "/");
-        scheduler.setInjector(injector);
-        scheduler.setNow(now );
-
-        try {
-            appendToLoggingTextArea("1");
-            scheduler.run();
-            appendToLoggingTextArea("2");
-            progressIndicator.setEnabled(false);
-        } catch (final PccException exception) {
-            appendToLoggingTextArea("3" + exception.toString());
-            LOGGER.error("", exception);
-            progressIndicator.setEnabled(false);
-            appendToLoggingTextArea("4");
-        }
-    }
-
-    private void appendToLoggingTextArea(final String aStuffToAppend) {
-        loggingTextArea.setValue(loggingTextArea.getValue() + NEWLINE
-                + aStuffToAppend);
-    }
+	private void appendToLoggingTextArea(final String aStuffToAppend) {
+		loggingTextArea.setValue(loggingTextArea.getValue() + NEWLINE
+				+ aStuffToAppend);
+	}
 }
