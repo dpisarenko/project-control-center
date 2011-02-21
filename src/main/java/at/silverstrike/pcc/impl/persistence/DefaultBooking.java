e272ee075e1f79502098ad828f3f2feb46cb549a
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-21 21:52:44 +0100

    Attempt at fixing checkstyle errors.

--- src/main/java/at/silverstrike/pcc/impl/persistence/DefaultBooking.java
+++ src/main/java/at/silverstrike/pcc/impl/persistence/DefaultBooking.java
@@ -19,49 +19,58 @@ import at.silverstrike.pcc.impl.util.Utils;
 
 /**
  * @author Dmitri Pisarenko
- *
+ * 
  */
 class DefaultBooking implements Booking {
+    private Long id;
+    private ControlProcess process;
+    private Resource resource;
+    private Date startDateTime;
+    private double duration;
 
-	private Long id;
-	private ControlProcess process;
-	private Resource resource;	
-	private Date startDateTime;	
-	private double duration;
-	public ControlProcess getProcess() {
-		return process;
-	}
-	public void setProcess(final ControlProcess process) {
-		this.process = process;
-	}
-	public Resource getResource() {
-		return resource;
-	}
-	public void setResource(final Resource resource) {
-		this.resource = resource;
-	}
-	public Date getStartDateTime() {
-		return startDateTime;
-	}
-	public void setStartDateTime(final Date startDateTime) {
-		this.startDateTime = startDateTime;
-	}
-	public double getDuration() {
-		return duration;
-	}
-	public void setDuration(final double duration) {
-		this.duration = duration;
-	}
-	@Override
-	public Date getEndDateTime() {
-	    return Utils.addHours(this.startDateTime, this.duration);
-	}
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(final Long id) {
-		this.id = id;
-	}
+    public ControlProcess getProcess() {
+        return process;
+    }
+
+    public void setProcess(final ControlProcess aProcess) {
+        this.process = aProcess;
+    }
+
+    public Resource getResource() {
+        return resource;
+    }
+
+    public void setResource(final Resource aResource) {
+        this.resource = aResource;
+    }
+
+    public Date getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(final Date aStartDateTime) {
+        this.startDateTime = aStartDateTime;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(final double aDuration) {
+        this.duration = aDuration;
+    }
+
+    @Override
+    public Date getEndDateTime() {
+        return Utils.addHours(this.startDateTime, this.duration);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long aId) {
+        this.id = aId;
+    }
 
 }
