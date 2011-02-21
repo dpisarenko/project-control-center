23c1b9cc5d946ad3eaed6dfefc4db4787944493f
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-21 21:07:55 +0100

    Attempt at fixing checkstyle errors.

--- src/main/java/at/silverstrike/pcc/api/debugids/DebugIdKeyNotFoundException.java
+++ src/main/java/at/silverstrike/pcc/api/debugids/DebugIdKeyNotFoundException.java
@@ -14,8 +14,7 @@ package at.silverstrike.pcc.api.debugids;
 public class DebugIdKeyNotFoundException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
-    public DebugIdKeyNotFoundException(final String aKey)
-    {
+    public DebugIdKeyNotFoundException(final String aKey) {
         super("Debug ID key not found: " + aKey);
     }
 }
