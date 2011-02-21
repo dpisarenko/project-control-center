8e162383dd42123c7f246e2454657c88aa723cbb
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-21 21:39:08 +0100

    Attempt at fixing checkstyle errors.

--- src/main/java/at/silverstrike/pcc/impl/mainprocesseditingpanel/ErrorCodes.java
+++ src/main/java/at/silverstrike/pcc/impl/mainprocesseditingpanel/ErrorCodes.java
@@ -1,12 +1,26 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
 package at.silverstrike.pcc.impl.mainprocesseditingpanel;
 
 import static at.silverstrike.pcc.api.conventions.MessageCodePrefixRegistry.Module.mainprocesseditingpanel;
 import at.silverstrike.pcc.api.conventions.MessageCodePrefixRegistry;
 
 final class ErrorCodes {
-    private final static String PREFIX = MessageCodePrefixRegistry
+    private static final String PREFIX = MessageCodePrefixRegistry
             .getInstance().getPrefix(mainprocesseditingpanel);
 
-    public final static String M_001_EXPORT_FAILURE = PREFIX + "001";
+    public static final String M_001_EXPORT_FAILURE = PREFIX + "001";
 
+    private ErrorCodes() {
+
+    }
 }
