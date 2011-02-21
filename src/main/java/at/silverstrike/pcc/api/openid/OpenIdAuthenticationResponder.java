c7afc654563f68a8846c32a517ffab9880909f56
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-21 21:18:57 +0100

    Attempt at fixing checkstyle errors.

--- src/main/java/at/silverstrike/pcc/api/openid/OpenIdAuthenticationResponder.java
+++ src/main/java/at/silverstrike/pcc/api/openid/OpenIdAuthenticationResponder.java
@@ -1,3 +1,14 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
 package at.silverstrike.pcc.api.openid;
 
 import javax.servlet.http.HttpServletRequest;
@@ -6,8 +17,11 @@ import javax.servlet.http.HttpServletResponse;
 import at.silverstrike.pcc.api.conventions.SingleActivityModule;
 
 public interface OpenIdAuthenticationResponder extends SingleActivityModule {
-	void setRequest(final HttpServletRequest aRequest);
-	void setResponse(final HttpServletResponse aResponse);
-	boolean isValidationSuccessful();
-	String getIdentity();
+    void setRequest(final HttpServletRequest aRequest);
+
+    void setResponse(final HttpServletResponse aResponse);
+
+    boolean isValidationSuccessful();
+
+    String getIdentity();
 }
