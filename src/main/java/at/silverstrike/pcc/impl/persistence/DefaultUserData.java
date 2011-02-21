eccbe5ea08b8f217be1e9ed591df9adda22a8863
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-21 21:56:49 +0100

    Attempt at fixing checkstyle errors.

--- src/main/java/at/silverstrike/pcc/impl/persistence/DefaultUserData.java
+++ src/main/java/at/silverstrike/pcc/impl/persistence/DefaultUserData.java
@@ -1,3 +1,14 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
 package at.silverstrike.pcc.impl.persistence;
 
 import java.util.List;
@@ -8,34 +19,41 @@ import at.silverstrike.pcc.api.model.DailyPlan;
 import at.silverstrike.pcc.api.model.UserData;
 
 class DefaultUserData implements UserData {
-	private String identifier;
-	private List<ControlProcess> processes;
-	private List<DailyPlan> dailyPlans;
-	private List<Booking> bookings;
+    private String identifier;
+    private List<ControlProcess> processes;
+    private List<DailyPlan> dailyPlans;
+    private List<Booking> bookings;
 
-	public String getIdentifier() {
-		return identifier;
-	}
-	public void setIdentifier(final String aIdentifier) {
-		this.identifier = aIdentifier;
-	}
-	public List<ControlProcess> getProcesses() {
-		return processes;
-	}
-	public void setProcesses(final List<ControlProcess> aProcesses) {
-		this.processes = aProcesses;
-	}
-	public List<DailyPlan> getDailyPlans() {
-		return dailyPlans;
-	}
-	public void setDailyPlans(final List<DailyPlan> aDailyPlans) {
-		this.dailyPlans = aDailyPlans;
-	}
-	public List<Booking> getBookings() {
-		return bookings;
-	}
-	public void setBookings(final List<Booking> aBookings) {
-		this.bookings = aBookings;
-	}
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(final String aIdentifier) {
+        this.identifier = aIdentifier;
+    }
+
+    public List<ControlProcess> getProcesses() {
+        return processes;
+    }
+
+    public void setProcesses(final List<ControlProcess> aProcesses) {
+        this.processes = aProcesses;
+    }
+
+    public List<DailyPlan> getDailyPlans() {
+        return dailyPlans;
+    }
+
+    public void setDailyPlans(final List<DailyPlan> aDailyPlans) {
+        this.dailyPlans = aDailyPlans;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(final List<Booking> aBookings) {
+        this.bookings = aBookings;
+    }
 
 }
