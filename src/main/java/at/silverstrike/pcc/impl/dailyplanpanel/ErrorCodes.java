a6bce2732601822e42a19259b2d7e56f4c816be8
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-21 21:10:00 +0100

    Attempt at fixing checkstyle errors.

--- src/main/java/at/silverstrike/pcc/impl/dailyplanpanel/ErrorCodes.java
+++ src/main/java/at/silverstrike/pcc/impl/dailyplanpanel/ErrorCodes.java
@@ -14,9 +14,14 @@ package at.silverstrike.pcc.impl.dailyplanpanel;
 import at.silverstrike.pcc.api.conventions.MessageCodePrefixRegistry;
 
 final class ErrorCodes {
-    private final static String PREFIX = MessageCodePrefixRegistry.getInstance().getPrefix(MessageCodePrefixRegistry.Module.dailyplanpanel);
-    
-    public final static String M_001_SELECTED_DAY_CHANGED = PREFIX + "001";
-    
-    public final static String M_002_SELECTED_DAY_CHANGED2 = PREFIX + "002";
+    private static final String PREFIX = MessageCodePrefixRegistry
+            .getInstance().getPrefix(
+                    MessageCodePrefixRegistry.Module.dailyplanpanel);
+
+    public static final String M_001_SELECTED_DAY_CHANGED = PREFIX + "001";
+
+    public static final String M_002_SELECTED_DAY_CHANGED2 = PREFIX + "002";
+
+    private ErrorCodes() {
+    }
 }
