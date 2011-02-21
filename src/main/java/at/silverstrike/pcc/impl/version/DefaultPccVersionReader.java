38c59b9993401a48886accfe596680c5c6f79477
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-21 13:38:48 +0100

    Attempt at fixing checkstyle errors.

--- src/main/java/at/silverstrike/pcc/impl/version/DefaultPccVersionReader.java
+++ src/main/java/at/silverstrike/pcc/impl/version/DefaultPccVersionReader.java
@@ -24,42 +24,39 @@ import at.silverstrike.pcc.api.conventions.PccException;
 import at.silverstrike.pcc.api.version.PccVersionReader;
 
 class DefaultPccVersionReader implements PccVersionReader {
-    private static final String PROPERTY_FILE_PATH = "version/application.properties";
-    private static final String VERSION_PROPERTY_NAME = "version";
-    private final Logger LOGGER =
-        LoggerFactory.getLogger(DefaultPccVersionReader.class);
-    private String version = "?";
-    
-    @Override
-    public String getVersion() {
-        return this.version;
-    }
+	private static final String PROPERTY_FILE_PATH = "version/application.properties";
+	private static final String VERSION_PROPERTY_NAME = "version";
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(DefaultPccVersionReader.class);
+	private String version = "?";
 
-    @Override
-    public void run() throws PccException {
-        final Properties properties = new Properties();
-        
-        final InputStream inputStream = getClass().getClassLoader().getResourceAsStream(PROPERTY_FILE_PATH);
-        try {
-            properties.load(inputStream);
-            final String versionFromFile = properties.getProperty(VERSION_PROPERTY_NAME);
-            
-            if (StringUtils.isNotBlank(versionFromFile))
-            {
-                this.version = versionFromFile;
-            }
+	@Override
+	public String getVersion() {
+		return this.version;
+	}
 
-        } catch (final IOException exception) {
-            LOGGER.error(ErrorCodes.M_001_RUN, exception);
-        }
-        catch (final RuntimeException exception)
-        {
-            LOGGER.error(ErrorCodes.M_002_RUN2, exception);
-        }
-        finally
-        {
-            IOUtils.closeQuietly(inputStream);
-        }
-    }
+	@Override
+	public void run() throws PccException {
+		final Properties properties = new Properties();
+
+		final InputStream inputStream = getClass().getClassLoader()
+				.getResourceAsStream(PROPERTY_FILE_PATH);
+		try {
+			properties.load(inputStream);
+			final String versionFromFile = properties
+					.getProperty(VERSION_PROPERTY_NAME);
+
+			if (StringUtils.isNotBlank(versionFromFile)) {
+				this.version = versionFromFile;
+			}
+
+		} catch (final IOException exception) {
+			LOGGER.error(ErrorCodes.M_001_RUN, exception);
+		} catch (final RuntimeException exception) {
+			LOGGER.error(ErrorCodes.M_002_RUN2, exception);
+		} finally {
+			IOUtils.closeQuietly(inputStream);
+		}
+	}
 
 }
