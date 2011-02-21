387c66e4e5ea23265fab75cedf662156162e964b
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-21 21:40:28 +0100

    Attempt at fixing checkstyle errors.

--- src/main/java/at/silverstrike/pcc/impl/openid/DefaultDeauthenticatorFactory.java
+++ src/main/java/at/silverstrike/pcc/impl/openid/DefaultDeauthenticatorFactory.java
@@ -1,3 +1,14 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
 package at.silverstrike.pcc.impl.openid;
 
 import at.silverstrike.pcc.api.openid.Deauthenticator;
@@ -5,9 +16,9 @@ import at.silverstrike.pcc.api.openid.DeauthenticatorFactory;
 
 public class DefaultDeauthenticatorFactory implements DeauthenticatorFactory {
 
-	@Override
-	public Deauthenticator create() {
-		return new DefaultDeauthenticator();
-	}
+    @Override
+    public final Deauthenticator create() {
+        return new DefaultDeauthenticator();
+    }
 
 }
