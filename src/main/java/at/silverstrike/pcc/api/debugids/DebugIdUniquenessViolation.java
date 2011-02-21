e94297638f10c1f891402318d2f7498ef909f210
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-21 13:26:52 +0100

    Attempt at fixing checkstyle errors.

--- src/main/java/at/silverstrike/pcc/api/debugids/DebugIdUniquenessViolation.java
+++ src/main/java/at/silverstrike/pcc/api/debugids/DebugIdUniquenessViolation.java
@@ -12,10 +12,9 @@
 package at.silverstrike.pcc.api.debugids;
 
 public class DebugIdUniquenessViolation extends RuntimeException {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public DebugIdUniquenessViolation(final String aKey)
-    {
-        super("DebugIdUniquenessViolation: " + aKey);
-    }
+	public DebugIdUniquenessViolation(final String aKey) {
+		super("DebugIdUniquenessViolation: " + aKey);
+	}
 }
