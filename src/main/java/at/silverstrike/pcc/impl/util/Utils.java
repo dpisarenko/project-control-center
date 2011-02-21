98a911b75f4f2f1527926dd3f7e5b8312f587c0a
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-21 13:38:23 +0100

    Attempt at fixing checkstyle errors.

--- src/main/java/at/silverstrike/pcc/impl/util/Utils.java
+++ src/main/java/at/silverstrike/pcc/impl/util/Utils.java
@@ -19,20 +19,26 @@ import org.apache.commons.lang.time.DateUtils;
 import com.vaadin.ui.TextField;
 
 public class Utils {
-    private static final int MINUTES_IN_HOUR = 60;
-    
-	public static boolean textFieldEmpty(final TextField aTextField)
-	{
-		final String text = (String)aTextField.getValue();
+	private static final int MINUTES_IN_HOUR = 60;
+
+	/**
+	 * We need this to fix checkstyle error: Utility classes should not have a
+	 * public or default constructor.
+	 */
+	private Utils() {
+	}
+
+	public static boolean textFieldEmpty(final TextField aTextField) {
+		final String text = (String) aTextField.getValue();
 		return isBlank(text);
 	}
-	
-    public static Date addHours(final Date aDateTime, final double anHours) {
-        final int hours = (int)anHours;
-        final int minutes = (int)((anHours - (double)hours)*MINUTES_IN_HOUR);
-        
-        final Date timeWithHoursAdded = DateUtils.addHours(aDateTime, hours);
-        
-        return DateUtils.addMinutes(timeWithHoursAdded, minutes);
-    }
+
+	public static Date addHours(final Date aDateTime, final double anHours) {
+		final int hours = (int) anHours;
+		final int minutes = (int) ((anHours - (double) hours) * MINUTES_IN_HOUR);
+
+		final Date timeWithHoursAdded = DateUtils.addHours(aDateTime, hours);
+
+		return DateUtils.addMinutes(timeWithHoursAdded, minutes);
+	}
 }
