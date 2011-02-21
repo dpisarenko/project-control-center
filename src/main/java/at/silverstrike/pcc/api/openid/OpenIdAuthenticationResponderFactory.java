ca07971434748c08c90b7b5c8ddf7d9a01058eaa
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-21 21:19:23 +0100

    Attempt at fixing checkstyle errors.

--- src/main/java/at/silverstrike/pcc/api/openid/OpenIdAuthenticationResponderFactory.java
+++ src/main/java/at/silverstrike/pcc/api/openid/OpenIdAuthenticationResponderFactory.java
@@ -1,3 +1,14 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
 package at.silverstrike.pcc.api.openid;
 
 import at.silverstrike.pcc.api.conventions.Factory;
