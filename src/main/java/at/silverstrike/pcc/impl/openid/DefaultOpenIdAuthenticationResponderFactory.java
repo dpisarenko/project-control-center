a113ae3e8e966f051256b0febb31d7b145a3e435
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-21 21:42:26 +0100

    Attempt at fixing checkstyle errors.

--- src/main/java/at/silverstrike/pcc/impl/openid/DefaultOpenIdAuthenticationResponderFactory.java
+++ src/main/java/at/silverstrike/pcc/impl/openid/DefaultOpenIdAuthenticationResponderFactory.java
@@ -1,6 +1,14 @@
 /**
+ * This file is part of Project Control Center (PCC).
  * 
- */
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
 package at.silverstrike.pcc.impl.openid;
 
 import at.silverstrike.pcc.api.openid.OpenIdAuthenticationResponder;
@@ -8,14 +16,14 @@ import at.silverstrike.pcc.api.openid.OpenIdAuthenticationResponderFactory;
 
 /**
  * @author DP118M
- *
+ * 
  */
 public class DefaultOpenIdAuthenticationResponderFactory implements
-		OpenIdAuthenticationResponderFactory {
+        OpenIdAuthenticationResponderFactory {
 
-	@Override
-	public OpenIdAuthenticationResponder create() {
-		return new DefaultOpenIdAuthenticationResponder();
-	}
+    @Override
+    public final OpenIdAuthenticationResponder create() {
+        return new DefaultOpenIdAuthenticationResponder();
+    }
 
 }
