e46645e0aabab050904757ba20831f133045fdc9
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-21 13:25:12 +0100

    Attempt at fixing checkstyle errors.

--- src/main/java/at/silverstrike/pcc/api/conventions/PccException.java
+++ src/main/java/at/silverstrike/pcc/api/conventions/PccException.java
@@ -13,12 +13,11 @@ package at.silverstrike.pcc.api.conventions;
 public class PccException extends Exception {
 	private static final long serialVersionUID = 1L;
 
-	public PccException(final String aMessage)
-	{
+	public PccException(final String aMessage) {
 		super(aMessage);
 	}
-	public PccException(final Throwable aThrowable)
-	{
+
+	public PccException(final Throwable aThrowable) {
 		super(aThrowable);
 	}
 }
