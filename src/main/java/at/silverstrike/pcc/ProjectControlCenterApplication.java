011c8f72aa06084ba67ed8d5940b4c84af71607c
Dmitri Pisarenko <dp@sw-dev.at>
2011-02-22 01:30:44 +0100

    Bugfix: java.lang.NullPointerException: null at at.silverstrike.pcc.impl.openid.HttpProcessor.setConsumerManager(HttpProcessor.java:87)

--- src/main/java/at/silverstrike/pcc/ProjectControlCenterApplication.java
+++ src/main/java/at/silverstrike/pcc/ProjectControlCenterApplication.java
@@ -28,11 +28,13 @@ import at.silverstrike.pcc.api.persistence.Persistence;
 import at.silverstrike.pcc.impl.injectorfactory.DefaultInjectorFactory;
 
 import com.google.inject.Injector;
+import com.vaadin.terminal.gwt.server.HttpServletRequestListener;
 
 import eu.livotov.tpt.TPTApplication;
 import eu.livotov.tpt.i18n.TM;
 
-public class ProjectControlCenterApplication extends TPTApplication {
+public class ProjectControlCenterApplication extends TPTApplication implements
+        HttpServletRequestListener {
     private static final Logger LOGGER = LoggerFactory
             .getLogger(ProjectControlCenterApplication.class);
     private static final String THEME = "pcc";
@@ -44,7 +46,7 @@ public class ProjectControlCenterApplication extends TPTApplication {
     private Injector injector;
     private EntryWindow entryWindow;
     private HttpServletRequest request;
-    
+
     @Override
     public final void close() {
         super.close();
@@ -78,6 +80,14 @@ public class ProjectControlCenterApplication extends TPTApplication {
                     .getInstance(EntryWindowFactory.class);
             entryWindow = entryWindowFactory.create();
 
+            if (this.request == null) {
+                LOGGER.debug("{}: request is null.",
+                        new Object[] { "000.001" });
+            } else {
+                LOGGER.debug("{}: request is not null.",
+                        new Object[] { "000.001" });
+            }
+
             entryWindow.setRequest(this.request);
             entryWindow.setInjector(injector);
             entryWindow.initGui();
@@ -102,38 +112,51 @@ public class ProjectControlCenterApplication extends TPTApplication {
 
     public final void onRequestStart(final HttpServletRequest aRequest,
             final HttpServletResponse aResponse) {
-        final OpenIdAuthenticationResponder responder = this.injector
-                .getInstance(OpenIdAuthenticationResponder.class);
 
         this.request = aRequest;
-        
-        responder.setRequest(aRequest);
-        responder.setResponse(aResponse);
 
-        try {
-            responder.run();
-        } catch (final PccException exception) {
-            LOGGER.error("", exception);
+        if (this.injector != null) {
+            final OpenIdAuthenticationResponder responder = this.injector
+                    .getInstance(OpenIdAuthenticationResponder.class);
+
+            LOGGER.debug("{}: request={}",
+                    new Object[] { "000.002", aRequest });
+
+            responder.setRequest(aRequest);
+            responder.setResponse(aResponse);
+
+            try {
+                responder.run();
+            } catch (final PccException exception) {
+                LOGGER.error("", exception);
+            }
+
+            if (responder.isValidationSuccessful()) {
+                // Go to the main page
+                final MainWindowFactory mainWindowFactory = injector
+                        .getInstance(MainWindowFactory.class);
+                final MainWindow mainWindow = mainWindowFactory.create();
+
+                mainWindow.setInjector(injector);
+                mainWindow.initGui();
+
+                this.setUser(responder.getIdentity());
+
+                setMainWindow(mainWindow.getWindow());
+
+            } else {
+                this.setUser(null);
+
+                // Go to the entry page
+                this.setMainWindow(this.entryWindow.getWindow());
+            }
         }
+    }
 
-        if (responder.isValidationSuccessful()) {
-            // Go to the main page
-            final MainWindowFactory mainWindowFactory = injector
-                    .getInstance(MainWindowFactory.class);
-            final MainWindow mainWindow = mainWindowFactory.create();
+    @Override
+    public void onRequestEnd(HttpServletRequest request,
+            HttpServletResponse response) {
+        // TODO Auto-generated method stub
 
-            mainWindow.setInjector(injector);
-            mainWindow.initGui();
-
-            this.setUser(responder.getIdentity());
-
-            setMainWindow(mainWindow.getWindow());
-
-        } else {
-            this.setUser(null);
-
-            // Go to the entry page
-            this.setMainWindow(this.entryWindow.getWindow());
-        }
     }
 }
