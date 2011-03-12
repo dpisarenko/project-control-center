635a7c3ac71bd0d3c919853995c220ec7b10863c
Dmitri Pisarenko <dp@sw-dev.at>
2011-03-12 16:08:30 +0100

    Attempts at fixing some checkstyle errors.

--- src/main/java/at/silverstrike/pcc/impl/centraleditingpanel/TempTreeObjectModel.java
+++ src/main/java/at/silverstrike/pcc/impl/centraleditingpanel/TempTreeObjectModel.java
@@ -1,11 +1,12 @@
 /**
  * This file is part of Project Control Center (PCC).
  * 
- * PCC (Project Control Center) project is intellectual property of Dmitri
- * Anatol'evich Pisarenko.
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
  * 
- * Copyright 2010 Dmitri Anatol'evich Pisarenko All rights reserved
- * 
+ * Copyright 2010 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
  **/
 
 package at.silverstrike.pcc.impl.centraleditingpanel;
