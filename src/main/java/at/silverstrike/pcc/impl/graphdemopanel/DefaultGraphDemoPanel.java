a9c391ea58e868ef603e2d3cae903436e797a972
Dmitri Pisarenko <dp@sw-dev.at>
2011-03-13 02:12:11 +0100

    Ready for testing JUNG graph display.

--- src/main/java/at/silverstrike/pcc/impl/graphdemopanel/DefaultGraphDemoPanel.java
+++ src/main/java/at/silverstrike/pcc/impl/graphdemopanel/DefaultGraphDemoPanel.java
@@ -12,19 +12,25 @@
 package at.silverstrike.pcc.impl.graphdemopanel;
 
 import java.awt.Dimension;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.apache.batik.svggen.SVGGraphics2D;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import edu.uci.ics.jung.algorithms.layout.CircleLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.SparseMultigraph;
 import edu.uci.ics.jung.graph.util.EdgeType;
-import edu.uci.ics.jung.visualization.BasicVisualizationServer;
 import edu.uci.ics.jung.visualization.VisualizationImageServer;
-import edu.uci.ics.jung.visualization.VisualizationServer;
 
 import com.vaadin.terminal.ThemeResource;
 import com.vaadin.ui.Embedded;
@@ -35,6 +41,8 @@ import at.silverstrike.pcc.api.graphdemopanel.GraphDemoPanel;
 
 class DefaultGraphDemoPanel extends Panel implements GraphDemoPanel {
     private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(DefaultGraphDemoPanel.class);
 
     @Override
     public Panel toPanel() {
@@ -46,75 +54,82 @@ class DefaultGraphDemoPanel extends Panel implements GraphDemoPanel {
         final VerticalLayout layout = new VerticalLayout();
         final Embedded image =
                 new Embedded(null, new ThemeResource("../pcc/test/graph.gif"));
-
         layout.addComponent(image);
         layout.setSizeFull();
 
         this.addComponent(layout);
+
+        createSampleGraph();
     }
 
     private void createSampleGraph() {
-        final Graph<String, String> graph = new SparseMultigraph<String, String>();
+        try {
+            DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory
+                    .newInstance();
+            DocumentBuilder docBuilder = null;
+            docBuilder = docBuilderFactory.newDocumentBuilder();
+            Document document = docBuilder.newDocument();
+            Element svgelem = document.createElement("svg");
+            document.appendChild(svgelem);
+
+            SVGGraphics2D graphic2d = new SVGGraphics2D(document);
+
+            final Graph<String, String> graph = createGraph();
+            final VisualizationImageServer<String, String> server =
+                    createServer(graph);
+
+            server.printAll(graphic2d);
+
+            Element el = graphic2d.getRoot();
+            el.setAttributeNS(null, "viewBox", "0 0 350 350");
+            el.setAttributeNS(null, "style", "width:100%;height:100%;");
+
+            ByteArrayOutputStream bout = new ByteArrayOutputStream();
+            Writer out = new OutputStreamWriter(bout, "UTF-8");
+
+            Embedded imageComponent = new Embedded("", new JungResource(bout));
+
+            imageComponent.setWidth(350, UNITS_PIXELS);
+            imageComponent.setHeight(350, UNITS_PIXELS);
+            imageComponent.setMimeType("image/svg+xml");
+            addComponent(imageComponent);
+        } catch (Exception exception) {
+            LOGGER.error("", exception);
+        }
+    }
+
+    private VisualizationImageServer<String, String> createServer(
+            Graph<String, String> graph) {
+        Layout<String, String> layout = new CircleLayout<String, String>(
+                graph);
+        layout.setSize(new Dimension(300, 300));
+        VisualizationImageServer<String, String> vv =
+                new VisualizationImageServer<String, String>(
+                        layout, new Dimension(350, 350));
+        return vv;
+    }
+
+    private Graph<String, String> createGraph() {
+        final Graph<String, String> graph =
+                new SparseMultigraph<String, String>();
         final String vertex1 = "IE";
         final String vertex2 = "P1";
         final String vertex3 = "P2";
         final String vertex4 = "P3";
         final String vertex5 = "FE";
-        
+
         graph.addVertex(vertex1);
         graph.addVertex(vertex2);
         graph.addVertex(vertex3);
         graph.addVertex(vertex4);
         graph.addVertex(vertex5);
-        
+
         graph.addEdge("", vertex1, vertex2, EdgeType.DIRECTED);
         graph.addEdge("", vertex2, vertex3, EdgeType.DIRECTED);
         graph.addEdge("", vertex3, vertex5, EdgeType.DIRECTED);
         graph.addEdge("", vertex1, vertex4, EdgeType.DIRECTED);
         graph.addEdge("", vertex4, vertex5, EdgeType.DIRECTED);
-        
-        final Layout<String, String> layout = new CircleLayout<String, String>(graph);
-        layout.setSize(new Dimension(300,300));
-        
-        
-        final VisualizationImageServer<String,String> vv =
-            new VisualizationImageServer<String,String>(layout, new Dimension(350,350));
-
-        
-        
-        SVGGraphics2D a = new SVGGraphics2D(arg0);
-        
-//        vv.printAll(graphic2d);
-
-        
-        
-        // ----
-        
-        
-        final VisualizationImageServer<String,String> vv =
-            new VisualizationImageServer<String,String>(layout, new Dimension(350,350));
-                
-        
-        BufferedImage bim = new BufferedImage(350, 350, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = bim.createGraphics();
-        viewFrame.paintAll(g);
-
-        
-        
-        vv.createImage(bim);
-        
-        g.dispose();
-        //this.viewFrame.paintComponents(g);
-        //try{Thread.sleep(1000);} catch(Exception e) {throw new RuntimeException(e);} // Sleeping doesn't help.
-        try {
-            File f = new File(filename);
-            ImageIO.write(bim,"png",f);
-            System.out.println("wrote image for " + jungGraph + " to "+ filename+ ":" + f.toString());
-            //try{Thread.sleep(500);} catch(Exception e) {throw new RuntimeException(e);} // Doesn't help
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
+        return graph;
     }
 
 }
---
+++ src/main/java/at/silverstrike/pcc/impl/graphdemopanel/JungResource.java
@@ -0,0 +1,90 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010, 2011 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
+package at.silverstrike.pcc.impl.graphdemopanel;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vaadin.Application;
+import com.vaadin.terminal.ApplicationResource;
+import com.vaadin.terminal.DownloadStream;
+
+import eu.livotov.tpt.TPTApplication;
+
+class JungResource implements ApplicationResource {
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(JungResource.class);
+
+    private ByteArrayInputStream bytestream = null;
+    private ByteArrayOutputStream originalOutputStream;
+
+    public JungResource(final ByteArrayOutputStream aOutputStream) {
+        this.originalOutputStream = aOutputStream;
+    }
+
+    @Override
+    public String getMIMEType() {
+        return "image/svg+xml";
+    }
+
+    @Override
+    public DownloadStream getStream() {
+        DownloadStream downloadStream = new DownloadStream(
+                getByteStream(), getMIMEType(), getFilename());
+        return downloadStream;
+
+    }
+
+    private InputStream getByteStream() {
+        this.bytestream = new ByteArrayInputStream(
+                originalOutputStream.toByteArray());
+        return this.bytestream;
+    }
+
+    @Override
+    public Application getApplication() {
+        return TPTApplication.getCurrentApplication();
+    }
+
+    @Override
+    public String getFilename() {
+        return "projectnetwork.svg";
+
+    }
+
+    @Override
+    public long getCacheTime() {
+        return 0;
+    }
+
+    @Override
+    public int getBufferSize() {
+        if (getByteStream() != null) {
+            try {
+                return getByteStream().available();
+            } catch (final IOException exception) {
+                LOGGER.error("", exception);
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+
+    }
+
+}
