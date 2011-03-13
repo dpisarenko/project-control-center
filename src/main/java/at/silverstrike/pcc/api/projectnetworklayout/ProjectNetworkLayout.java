9d1e633f79e006d347c29f3329b14d0f80c46999
Dmitri Pisarenko <dp@sw-dev.at>
2011-03-13 20:14:25 +0100

    Initial commit.

---
+++ src/main/java/at/silverstrike/pcc/api/projectnetworklayout/ProjectNetworkLayout.java
@@ -0,0 +1,20 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010, 2011 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
+package at.silverstrike.pcc.api.projectnetworklayout;
+
+/**
+ * @author DP118M
+ *
+ */
+public interface ProjectNetworkLayout {
+
+}
