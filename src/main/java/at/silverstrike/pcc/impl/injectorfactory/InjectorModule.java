808f0b7e428221d516bd96a647ed87226e52b7ae
Dmitri Pisarenko <dp@sw-dev.at>
2011-03-18 13:01:10 +0100

    Attempt at bugfix: 1) No implementation for at.silverstrike.pcc.api.centraleditingpanelcontroller.CentralEditingPanelControllerFactory was bound.

--- src/main/java/at/silverstrike/pcc/impl/injectorfactory/InjectorModule.java
+++ src/main/java/at/silverstrike/pcc/impl/injectorfactory/InjectorModule.java
@@ -15,6 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import at.silverstrike.pcc.api.centraleditingpanel.CentralEditingPanelFactory;
+import at.silverstrike.pcc.api.centraleditingpanelcontroller.CentralEditingPanelControllerFactory;
 import at.silverstrike.pcc.api.conventions.PccException;
 import at.silverstrike.pcc.api.culture2lang.CultureToLanguageMapper;
 import at.silverstrike.pcc.api.culture2lang.CultureToLanguageMapperFactory;
