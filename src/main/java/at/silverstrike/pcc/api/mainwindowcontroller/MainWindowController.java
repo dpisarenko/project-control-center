b1603a37d10ce8775f70d24133723473b5b2021b
Dmitri Pisarenko <dp@sw-dev.at>
2011-03-24 20:58:41 +0100

    Fixed some checkstyle errors.

--- src/main/java/at/silverstrike/pcc/api/mainwindowcontroller/MainWindowController.java
+++ src/main/java/at/silverstrike/pcc/api/mainwindowcontroller/MainWindowController.java
@@ -1,9 +1,20 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010, 2011 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
 package at.silverstrike.pcc.api.mainwindowcontroller;
 
 import at.silverstrike.pcc.api.conventions.ModuleWithInjectableDependencies;
 
 public interface MainWindowController extends ModuleWithInjectableDependencies {
-	void importFromXML();
-	void exportToXML();
+    void importFromXML();
 
+    void exportToXML();
 }
--- src/main/java/at/silverstrike/pcc/impl/centraleditingpanel/DefaultCentralEditingPanel.java
+++ src/main/java/at/silverstrike/pcc/impl/centraleditingpanel/DefaultCentralEditingPanel.java
@@ -10,10 +10,6 @@
  **/
 
 package at.silverstrike.pcc.impl.centraleditingpanel;
-
-import java.util.Arrays;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,16 +17,12 @@ import com.google.inject.Injector;
 import com.vaadin.data.util.HierarchicalContainer;
 import com.vaadin.terminal.Sizeable;
 import com.vaadin.terminal.ThemeResource;
-import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.Embedded;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.GridLayout;
-import com.vaadin.ui.Table;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.Tree;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
@@ -39,25 +31,16 @@ import com.vaadin.ui.Button.ClickListener;
 import at.silverstrike.pcc.api.centraleditingpanel.CentralEditingPanel;
 import at.silverstrike.pcc.api.centraleditingpanelcontroller.CentralEditingPanelController;
 import at.silverstrike.pcc.api.centraleditingpanelcontroller.CentralEditingPanelControllerFactory;
-import at.silverstrike.pcc.api.conventions.AbstractedPanel;
 import at.silverstrike.pcc.api.conventions.MessageCodePrefixRegistry;
-import at.silverstrike.pcc.api.conventions.PccException;
 import at.silverstrike.pcc.api.debugids.DebugIdRegistry;
-import at.silverstrike.pcc.api.meetingeditingpanel.MeetingEditingPanel;
-import at.silverstrike.pcc.api.meetingeditingpanel.MeetingEditingPanelFactory;
-import at.silverstrike.pcc.api.milestoneeditingpanel.MilestoneEditingPanel;
-import at.silverstrike.pcc.api.milestoneeditingpanel.MilestoneEditingPanelFactory;
 import at.silverstrike.pcc.api.processpanel.ProcessPanelListener;
-import at.silverstrike.pcc.api.taskeditingpanel.TaskEditingPanel;
-import at.silverstrike.pcc.api.taskeditingpanel.TaskEditingPanelFactory;
-import at.silverstrike.pcc.api.testtablecreator.TestTableCreator;
 import at.silverstrike.pcc.impl.centraleditingpanelcontroller.DefaultCentralEditingPanelControllerFactory;
 import eu.livotov.tpt.i18n.TM;
 
 class DefaultCentralEditingPanel extends Panel implements
         CentralEditingPanel, ProcessPanelListener, ClickListener {
     private static final String NEW_MILESTONE_BUTTON = "025.002";
-	private static final int PADDING = 5;
+    private static final int PADDING = 5;
     private static final int ONE_SIXTH_OF_SCREEN_WIDTH = 6;
     private static final Logger LOGGER = LoggerFactory
             .getLogger(DefaultCentralEditingPanel.class);
@@ -70,24 +53,11 @@ class DefaultCentralEditingPanel extends Panel implements
 
     public static final Object PROJECT_PROPERTY_NAME = "name";
 
-    private static final int PROCESS_NAME_TEXT_FIELD_COLUMNS = 30;
-    private static final int PROCESS_NAME_TEXT_FIELD_ROWS = 5;
-
-    private static final String[] DURATION_STEPS = new String[] { "15 min",
-            "30 min",
-            "45 min" };
-    private static final String[] TEST_COLUMN_NAMES = new String[] { "№",
-            "Project", "Name" };
-    private static final List<String[]> TEST_TABLE_DATA =
-            Arrays.asList(
-                    new String[] { "1.1", "Project 1", "Task 1" },
-                    new String[] { "2.1", "Project 4", "Task 5" });
-    
     private Panel verticalPanelRight;
 
     private transient Injector injector;
     private transient CentralEditingPanelController controller;
-    
+
     private DebugIdRegistry debugIdRegistry;
 
     @Override
@@ -109,8 +79,8 @@ class DefaultCentralEditingPanel extends Panel implements
 
     @Override
     public void initGui() {
-    	this.debugIdRegistry = this.injector.getInstance(DebugIdRegistry.class);
-    	
+        this.debugIdRegistry = this.injector.getInstance(DebugIdRegistry.class);
+
         final GridLayout mainGrid = new GridLayout(2, 1);
 
         mainGrid.setWidth(WIDTH_SCREEN, Sizeable.UNITS_PIXELS);
@@ -138,7 +108,8 @@ class DefaultCentralEditingPanel extends Panel implements
 
         final Button newMilestoneButton = getNewMilestoneButton();
         newMilestoneButton.setDebugId(this.debugIdRegistry
-                .getDebugId(MessageCodePrefixRegistry.Module.centraleditingpanel,
+                .getDebugId(
+                        MessageCodePrefixRegistry.Module.centraleditingpanel,
                         "2-button-newMilestone"));
         buttonsNewGrid.addComponent(newMilestoneButton, 2, 0);
         buttonsNewGrid.setComponentAlignment(newMilestoneButton,
@@ -173,337 +144,25 @@ class DefaultCentralEditingPanel extends Panel implements
         tree.setContainerDataSource(getFilterHierarchicalContainer());
 
         mainGrid.addComponent(verticalLayoutLeft, 0, 0);
-/*
-        final VerticalLayout verticalLayoutRight = new VerticalLayout();
 
-        final Label taskLabel =
-                new Label(TM.get("centraleditingprocesspanel.10-label-task"));
-        taskLabel.setContentMode(Label.CONTENT_TEXT);
-        verticalLayoutRight.addComponent(taskLabel);
-
-        final HorizontalLayout buttonsTaskLayout = new HorizontalLayout();
-        buttonsTaskLayout.setSpacing(true);
-
-        final Button saveButton =
-                new Button(TM.get("centraleditingprocesspanel.11-button-save"));
-        saveButton.addListener(this); // react to clicks
-        buttonsTaskLayout.addComponent(saveButton);
-
-        final Button doneButton =
-                new Button(TM.get("centraleditingprocesspanel.12-button-done"));
-        doneButton.addListener(this); // react to clicks
-        buttonsTaskLayout.addComponent(doneButton);
-
-        final Button deleteButton =
-                new Button(
-                        TM.get("centraleditingprocesspanel.13-button-delete"));
-        deleteButton.addListener(this); // react to clicks
-        buttonsTaskLayout.addComponent(deleteButton);
-
-        verticalLayoutRight.addComponent(buttonsTaskLayout);
-
-        final TextField taskNameTextField = new TextField();
-        taskNameTextField.setColumns(PROCESS_NAME_TEXT_FIELD_COLUMNS);
-        taskNameTextField.setRows(PROCESS_NAME_TEXT_FIELD_ROWS);
-        verticalLayoutRight.addComponent(taskNameTextField);
-
-        final Label effortLabel =
-                new Label(TM.get("centraleditingprocesspanel.14-label-effort"));
-        effortLabel.setContentMode(Label.CONTENT_TEXT);
-        verticalLayoutRight.addComponent(effortLabel);
-
-        final HorizontalLayout effortLayout = getEffortPanel();
-
-        verticalLayoutRight.addComponent(effortLayout);
-
-        final HorizontalLayout dependLayout = new HorizontalLayout();
-        dependLayout.setSpacing(true);
-
-        final Label dependLabel =
-                new Label(
-                        TM.get("centraleditingprocesspanel.17-label-dependencies"));
-        dependLayout.addComponent(dependLabel);
-
-        final Button dependEditButton = createDependEditButton(this.controller);
-        dependLayout.addComponent(dependEditButton);
-
-        verticalLayoutRight.addComponent(dependLayout);
-
-        final Table table = createTestTable();
-        verticalLayoutRight.addComponent(table);
-*/
-        //final VerticalLayout verticalLayoutRight = getTaskLayout();
-        
-        //final AbstractedPanel panel = getTaskEditingPanel();
-        //final AbstractedPanel panel = getMeetingEditingPanel();
-        //final AbstractedLayout panel = getMilestoneEditingPanel();
-        //final Panel verticalPanelRight = panel.toPanel();
         controller = getController();
-        //verticalPanelRight = controller.getTaskPanel();
         verticalPanelRight = controller.getMeetingPanel();
-        //verticalPanelRight = controller.getMilestonePanel();
-              
+
         mainGrid.addComponent(verticalPanelRight, 1, 0);
 
         this.addComponent(mainGrid);
     }
 
-	private CentralEditingPanelController getController() {
-		final DefaultCentralEditingPanelControllerFactory factory =
-            this.injector.getInstance(DefaultCentralEditingPanelControllerFactory.class);
+    private CentralEditingPanelController getController() {
+        final DefaultCentralEditingPanelControllerFactory factory =
+                this.injector
+                        .getInstance(DefaultCentralEditingPanelControllerFactory.class);
         final CentralEditingPanelController controller = factory.create();
         controller.setInjector(this.injector);
         return controller;
-	}
-/*
-    private VerticalLayout getTaskLayout() {
-        final VerticalLayout verticalLayoutRight = new VerticalLayout();
-
-        final Label taskLabel =
-                new Label(TM.get("centraleditingprocesspanel.10-label-task"));
-        taskLabel.setContentMode(Label.CONTENT_TEXT);
-        verticalLayoutRight.addComponent(taskLabel);
-
-        final HorizontalLayout buttonsTaskLayout = new HorizontalLayout();
-        buttonsTaskLayout.setSpacing(true);
-
-        final Button saveButton =
-                        new Button(
-                                TM.get("centraleditingprocesspanel.11-button-save"));
-        saveButton.addListener(this); // react to clicks
-        buttonsTaskLayout.addComponent(saveButton);
-
-        final Button doneButton =
-                new Button(TM.get("centraleditingprocesspanel.12-button-done"));
-        doneButton.addListener(this); // react to clicks
-        buttonsTaskLayout.addComponent(doneButton);
-
-        final Button deleteButton =
-                        new Button(
-                                TM.get("centraleditingprocesspanel.13-button-delete"));
-        deleteButton.addListener(this); // react to clicks
-        buttonsTaskLayout.addComponent(deleteButton);
-
-        verticalLayoutRight.addComponent(buttonsTaskLayout);
-
-        final TextField taskNameTextField = new TextField();
-        taskNameTextField.setColumns(PROCESS_NAME_TEXT_FIELD_COLUMNS);
-        taskNameTextField.setRows(PROCESS_NAME_TEXT_FIELD_ROWS);
-        verticalLayoutRight.addComponent(taskNameTextField);
-
-        final Label effortLabel =
-                new Label(TM.get("centraleditingprocesspanel.14-label-effort"));
-        effortLabel.setContentMode(Label.CONTENT_TEXT);
-        verticalLayoutRight.addComponent(effortLabel);
-        final HorizontalLayout effortLayout = getEffortPanel();
-
-        verticalLayoutRight.addComponent(effortLayout);
-
-        final HorizontalLayout dependLayout = new HorizontalLayout();
-        dependLayout.setSpacing(true);
-
-        final Label dependLabel =
-                 new Label(
-                         TM.get("centraleditingprocesspanel.17-label-dependencies"));
-        dependLayout.addComponent(dependLabel);
-
-        final Button dependEditButton = createDependEditButton(this.controller);
-        dependLayout.addComponent(dependEditButton);
-
-        verticalLayoutRight.addComponent(dependLayout);
-
-        final Table table = createTestTable();
-        verticalLayoutRight.addComponent(table);
-        return verticalLayoutRight;
     }
 
-    private VerticalLayout getMeetingLayout() {
-        final VerticalLayout verticalLayoutRight = new VerticalLayout();
 
-        final Label taskLabel =
-                new Label(TM.get("centraleditingprocesspanel.20-label-meeting"));
-        taskLabel.setContentMode(Label.CONTENT_TEXT);
-        verticalLayoutRight.addComponent(taskLabel);
-
-        final HorizontalLayout buttonsTaskLayout = new HorizontalLayout();
-        buttonsTaskLayout.setSpacing(true);
-
-        final Button saveButton =
-                new Button(TM.get("centraleditingprocesspanel.11-button-save"));
-        saveButton.addListener(this); // react to clicks
-        buttonsTaskLayout.addComponent(saveButton);
-
-        final Button deleteButton =
-                new Button(
-                        TM.get("centraleditingprocesspanel.13-button-delete"));
-        deleteButton.addListener(this); // react to clicks
-        buttonsTaskLayout.addComponent(deleteButton);
-
-        verticalLayoutRight.addComponent(buttonsTaskLayout);
-
-        final TextField taskNameTextField = new TextField();
-        taskNameTextField.setColumns(PROCESS_NAME_TEXT_FIELD_COLUMNS);
-        taskNameTextField.setRows(PROCESS_NAME_TEXT_FIELD_ROWS);
-        verticalLayoutRight.addComponent(taskNameTextField);
-
-        final HorizontalLayout placeLayout = getPlacePanel();
-
-        verticalLayoutRight.addComponent(placeLayout);
-
-        final HorizontalLayout dependLayout = new HorizontalLayout();
-        dependLayout.setSpacing(true);
-
-        final Label dependLabel =
-                new Label(
-                        TM.get("centraleditingprocesspanel.17-label-dependencies"));
-        dependLayout.addComponent(dependLabel);
-
-        final Button dependEditButton = createDependEditButton(this.controller);
-        dependLayout.addComponent(dependEditButton);
-
-        verticalLayoutRight.addComponent(dependLayout);
-
-        final Table table = createTestTable();
-        verticalLayoutRight.addComponent(table);
-        return verticalLayoutRight;
-    }
-
-    private VerticalLayout getMilestoneLayout() {
-        final VerticalLayout verticalLayoutRight = new VerticalLayout();
-
-        final Label taskLabel =
-                new Label(
-                        TM.get("centraleditingprocesspanel.21-label-milestone"));
-        taskLabel.setContentMode(Label.CONTENT_TEXT);
-        verticalLayoutRight.addComponent(taskLabel);
-
-        final HorizontalLayout buttonsTaskLayout = new HorizontalLayout();
-        buttonsTaskLayout.setSpacing(true);
-
-        final Button saveButton =
-                new Button(TM.get("centraleditingprocesspanel.11-button-save"));
-        saveButton.addListener(this); // react to clicks
-        buttonsTaskLayout.addComponent(saveButton);
-
-        final Button doneButton =
-                new Button(TM.get("centraleditingprocesspanel.12-button-done"));
-        doneButton.addListener(this); // react to clicks
-        buttonsTaskLayout.addComponent(doneButton);
-
-        final Button deleteButton =
-                new Button(
-                        TM.get("centraleditingprocesspanel.13-button-delete"));
-        deleteButton.addListener(this); // react to clicks
-        buttonsTaskLayout.addComponent(deleteButton);
-
-        verticalLayoutRight.addComponent(buttonsTaskLayout);
-
-        final TextField taskNameTextField = new TextField();
-        taskNameTextField.setColumns(PROCESS_NAME_TEXT_FIELD_COLUMNS);
-        taskNameTextField.setRows(PROCESS_NAME_TEXT_FIELD_ROWS);
-        verticalLayoutRight.addComponent(taskNameTextField);
-
-        final Label effortLabel =
-                new Label(TM.get("centraleditingprocesspanel.14-label-effort"));
-        effortLabel.setContentMode(Label.CONTENT_TEXT);
-        verticalLayoutRight.addComponent(effortLabel);
-
-        final HorizontalLayout effortLayout = getEffortPanel();
-
-        verticalLayoutRight.addComponent(effortLayout);
-
-        final HorizontalLayout dependLayout = new HorizontalLayout();
-        dependLayout.setSpacing(true);
-
-        final Label dependLabel =
-                new Label(
-                        TM.get("centraleditingprocesspanel.17-label-dependencies"));
-        dependLayout.addComponent(dependLabel);
-
-        final Button dependEditButton = createDependEditButton(this.controller);
-        dependLayout.addComponent(dependEditButton);
-
-        verticalLayoutRight.addComponent(dependLayout);
-
-        final Table table = createTestTable();
-        verticalLayoutRight.addComponent(table);
-        return verticalLayoutRight;
-    }
-    
-	private TaskEditingPanel getTaskEditingPanel() {
-		final TaskEditingPanelFactory factory =
-            this.injector.getInstance(TaskEditingPanelFactory.class);
-        final TaskEditingPanel panel = factory.create();
-        panel.setInjector(this.injector);
-		return panel;
-	}
-	
-	private MeetingEditingPanel getMeetingEditingPanel() {
-		final MeetingEditingPanelFactory factory =
-            this.injector.getInstance(MeetingEditingPanelFactory.class);
-        final MeetingEditingPanel panel = factory.create();
-        panel.setInjector(this.injector);
-		return panel;
-	}
-	
-	private MilestoneEditingPanel getMilestoneEditingPanel() {
-		final MilestoneEditingPanelFactory factory =
-            this.injector.getInstance(MilestoneEditingPanelFactory.class);
-        final MilestoneEditingPanel panel = factory.create();
-        panel.setInjector(this.injector);
-		return panel;
-	}
-*/
-    private HorizontalLayout getEffortPanel() {
-        final HorizontalLayout effortLayout = new HorizontalLayout();
-        effortLayout.setSpacing(true);
-
-        final Label fromLabel =
-                new Label(TM.get("centraleditingprocesspanel.15-label-from"));
-        fromLabel.setContentMode(Label.CONTENT_TEXT);
-        effortLayout.addComponent(fromLabel);
-
-        final ComboBox from = new ComboBox();
-        for (int i = 0; i < DURATION_STEPS.length - 1; i++) {
-            from.addItem(DURATION_STEPS[i]);
-        }
-        effortLayout.addComponent(from);
-
-        final Label toLabel =
-                new Label(TM.get("centraleditingprocesspanel.16-label-to"));
-        toLabel.setContentMode(Label.CONTENT_TEXT);
-        effortLayout.addComponent(toLabel);
-
-        final ComboBox to = new ComboBox();
-        for (int i = 1; i < DURATION_STEPS.length; i++) {
-            to.addItem(DURATION_STEPS[i]);
-        }
-        effortLayout.addComponent(to);
-        return effortLayout;
-    }
-
-    private Button createDependEditButton(
-            final CentralEditingPanelController aController) {
-        final Button dependEditButton =
-                new Button(TM.get("centraleditingprocesspanel.18-button-edit"));
-        dependEditButton.addListener(new DependenciesButtonClickListener(
-                aController));
-        return dependEditButton;
-    }
-
-    private Table createTestTable() {
-        final TestTableCreator creator =
-                this.injector.getInstance(TestTableCreator.class);
-        creator.setColumnNames(TEST_COLUMN_NAMES);
-        creator.setData(TEST_TABLE_DATA);
-        try {
-            creator.run();
-        } catch (final PccException exception) {
-            LOGGER.error(ErrorCodes.M_001_TEST_TABLE_CREATION, exception);
-        }
-        final Table table = creator.getTable();
-        return table;
-    }
 
     private VerticalLayout getTreeLayout() {
         final VerticalLayout treeLayout = new VerticalLayout();
@@ -548,14 +207,10 @@ class DefaultCentralEditingPanel extends Panel implements
      * Shows a notification when a button is clicked.
      */
     public void buttonClick(final ClickEvent aEvent) {
-    	if (aEvent.getButton().getDebugId().equals(NEW_MILESTONE_BUTTON))
-    			//this.debugIdRegistry
-                //.getDebugId(MessageCodePrefixRegistry.Module.centraleditingpanel,
-                //"2-button-newMilestone")))
-    	{
-    		getWindow().showNotification("lalala ");
-    		//verticalPanelRight = controller.getMilestonePanel();    		    		
-    	}
+        if (NEW_MILESTONE_BUTTON.equals(aEvent.getButton().getDebugId()))
+        {
+            getWindow().showNotification("lalala ");
+        }
         getWindow().showNotification(NOTIFICATION);
     }
 
