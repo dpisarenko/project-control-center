301563ab858421ee35345e6e66f5ff76d018b271
Dmitri Pisarenko <dp@sw-dev.at>
2011-04-05 17:16:55 +0200

    Initial commit of localization test case.

---
+++ src/test/java/at/silverstrike/pcc/test/i18n/ProblemTuples.java
@@ -0,0 +1,27 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010, 2011 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
+package at.silverstrike.pcc.test.i18n;
+
+import java.util.List;
+
+/**
+ * @author DP118M
+ *
+ */
+class ProblemTuples {
+
+    public List<String> getProblematicKeys() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
