6f2d7cefc356043d0238891dcb0c6488ca4f732e
Dmitri Pisarenko <dp@sw-dev.at>
2011-04-09 18:11:09 +0200

    Fixed checkstyle errors.

--- src/test/java/at/silverstrike/pcc/test/tj3bookingsparser/TestDefaultBookingsFile2Bookings.java
+++ src/test/java/at/silverstrike/pcc/test/tj3bookingsparser/TestDefaultBookingsFile2Bookings.java
@@ -37,6 +37,15 @@ import at.silverstrike.pcc.test.testutils.MockInjectorFactory;
 import com.google.inject.Injector;
 
 public final class TestDefaultBookingsFile2Bookings {
+    private static final int FORTY_FIVE_MINUTES = 45;
+    private static final int ELEVEN_O_CLOCK = 11;
+    private static final int NINE_O_CLOCK = 9;
+    private static final int DAY_25 = 25;
+    private static final int YEAR_2010 = 2010;
+    private static final double DURATION_2_75_HOURS = 2.75;
+    private static final long RESOURCE_ID = 1210L;
+    private static final long PROCESS_ID = 2010L;
+
     @Before
     public void setupLogger() {
     }
@@ -45,7 +54,7 @@ public final class TestDefaultBookingsFile2Bookings {
     public void test01() {
         /**
          * Create the injector
-         */        
+         */
         final InjectorFactory injectorFactory =
                 new MockInjectorFactory(new MockInjectorModule());
         final Injector injector = injectorFactory.createInjector();
@@ -53,36 +62,36 @@ public final class TestDefaultBookingsFile2Bookings {
         final BookingsFile2BookingsFactory factory =
                 injector.getInstance(BookingsFile2BookingsFactory.class);
         final BookingsFile2Bookings objectUnderTest = factory.create();
-        
+
         Assert.assertNotNull(objectUnderTest);
-        
+
         /**
          * Prepare input data
          */
         final BookingsFile bookingsFile = new MockBookingsFile();
         final MockSupplementStatement suppStmt = new MockSupplementStatement();
         final MockIndBooking indBooking = new MockIndBooking();
-        final MockBookingStatement bookingStatement = new MockBookingStatement();
-        
-        
+        final MockBookingStatement bookingStatement =
+                new MockBookingStatement();
+
         indBooking.setDuration("2.75h");
         indBooking.setStartTime("2010-10-25-09:00-+0200");
-        
+
         Assert.assertNotNull(bookingStatement.getIndBookings());
-        
+
         bookingStatement.getIndBookings().add(indBooking);
         bookingStatement.setResource("R1210");
         bookingStatement.setScenario("");
-        
+
         Assert.assertNotNull(suppStmt.getBookingStatements());
 
         suppStmt.getBookingStatements().add(bookingStatement);
         suppStmt.setTaskId("T2010");
-        
+
         Assert.assertNotNull(bookingsFile.getSupplementStatements());
-        
+
         bookingsFile.getSupplementStatements().add(suppStmt);
-        
+
         /**
          * Run method under test
          */
@@ -95,35 +104,39 @@ public final class TestDefaultBookingsFile2Bookings {
         } catch (final ParseException exception) {
             Assert.fail(exception.getMessage());
         }
-        
+
         /**
          * Evaluate results
          */
         final List<BookingTuple> tuples = objectUnderTest.getTuples();
-        
+
         Assert.assertNotNull(tuples);
         Assert.assertEquals(1, tuples.size());
-        
+
         final BookingTuple tuple = tuples.get(0);
-        
-        Assert.assertEquals(2010L, tuple.getProcessId());
-        Assert.assertEquals(1210L, tuple.getResourceId());
-        
+
+        Assert.assertEquals(PROCESS_ID, tuple.getProcessId());
+        Assert.assertEquals(RESOURCE_ID, tuple.getResourceId());
+
         final Booking booking = tuple.getBooking();
-        
+
         Assert.assertNotNull(booking);
-        
-        Assert.assertEquals(2.75, booking.getDuration(), TestConventions.DELTA);
-        
-        Assert.assertEquals(RubyDateTimeUtils.getDate(2010, OCTOBER, 25, 9, 0), booking.getStartDateTime());
-        Assert.assertEquals(RubyDateTimeUtils.getDate(2010, OCTOBER, 25, 11, 45), booking.getEndDateTime());
-        
+
+        Assert.assertEquals(DURATION_2_75_HOURS, booking.getDuration(),
+                TestConventions.DELTA);
+
+        Assert.assertEquals(RubyDateTimeUtils.getDate(YEAR_2010, OCTOBER,
+                DAY_25, NINE_O_CLOCK, 0), booking.getStartDateTime());
+        Assert.assertEquals(RubyDateTimeUtils.getDate(YEAR_2010, OCTOBER,
+                DAY_25, ELEVEN_O_CLOCK, FORTY_FIVE_MINUTES), booking
+                .getEndDateTime());
+
         final Task process = booking.getProcess();
-        
+
         Assert.assertNull(process);
-        
+
         final Resource resource = booking.getResource();
-        
+
         Assert.assertNull(resource);
     }
 }
