1bbd9f3b77e3a1774ed80fac0720c18ea07bdc50
Dmitri Pisarenko <dp@sw-dev.at>
2011-04-10 19:25:14 +0200

    Initial commit.

---
+++ src/test/java/at/silverstrike/pcc/test/model/MockEvent.java
@@ -0,0 +1,46 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010, 2011 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
+package at.silverstrike.pcc.test.model;
+
+import java.util.Date;
+
+import at.silverstrike.pcc.api.model.Event;
+
+/**
+ * @author DP118M
+ *
+ */
+class MockEvent extends MockSchedulingObject implements Event {
+    private String place;
+    private Date startDateTime;
+    private Date endDateTime;
+    
+    public String getPlace() {
+        return place;
+    }
+    public void setPlace(final String place) {
+        this.place = place;
+    }
+    public Date getStartDateTime() {
+        return startDateTime;
+    }
+    public void setStartDateTime(final Date startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+    public Date getEndDateTime() {
+        return endDateTime;
+    }
+    public void setEndDateTime(final Date endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+
+}
---
+++ src/test/java/at/silverstrike/pcc/test/model/MockSchedulingObject.java
@@ -0,0 +1,90 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010, 2011 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
+package at.silverstrike.pcc.test.model;
+
+import java.util.Set;
+
+import at.silverstrike.pcc.api.model.ProcessState;
+import at.silverstrike.pcc.api.model.SchedulingObject;
+
+/**
+ * @author DP118M
+ *
+ */
+class MockSchedulingObject {
+    private Long id;
+    private String name;
+    private ProcessState state;
+    private Integer priority;
+    private SchedulingObject parentProcess;
+    private Set<SchedulingObject> predecessors;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    protected void setId(final Long aId) {
+        this.id = aId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(final String aName) {
+        this.name = aName;
+    }
+
+    public ProcessState getState() {
+        return this.state;
+    }
+
+    public void setState(final ProcessState aState) {
+        this.state = aState;
+    }
+
+    public void setPriority(final Integer aPriority) {
+        this.priority = aPriority;
+    }
+
+    public Integer getPriority() {
+        return this.priority;
+    }
+
+    public SchedulingObject getParent() {
+        return this.parentProcess;
+    }
+
+    public void setParent(final SchedulingObject aParentProcess) {
+        this.parentProcess = aParentProcess;
+    }
+
+    public Set<SchedulingObject> getPredecessors() {
+        return predecessors;
+    }
+
+    public void setPredecessors(final Set<SchedulingObject> aPredecessors) {
+        this.predecessors = aPredecessors;
+    }
+
+    public void setLabel(final String aLabel) {
+    }
+
+    public String getLabel() {
+        if (this.id != null) {
+            return this.id.toString();
+        } else {
+            return "";
+        }
+    }
+
+}
