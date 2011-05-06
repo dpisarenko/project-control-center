f18f6c6c3d43b98765a182a7ff84f996ae5491a4
Dmitri Pisarenko <dp@sw-dev.at>
2011-05-06 18:29:32 +0200

    Attempt at making logging work with Apache Tomcat 7.

--- src/main/java/at/silverstrike/pcc/impl/persistence/DefaultPersistence.java
+++ src/main/java/at/silverstrike/pcc/impl/persistence/DefaultPersistence.java
@@ -59,10 +59,15 @@ public class DefaultPersistence implements Persistence {
     private static final int LAST_MILLISECOND = 999;
     public static final String DB_NAME = "pcc";
     private static final int DAYS_TO_PLAN_AHEAD = 7;
-    private static final String JDBC_CONN_STRING_EXISTING_DB = "jdbc:derby:"
-            + DB_NAME;
-    private static final String JDBC_CONN_STRING_NEW_DB = "jdbc:derby:"
-            + DB_NAME + ";create=true";
+    // private static final String JDBC_CONN_STRING_EXISTING_DB = "jdbc:derby:"
+    // + DB_NAME;
+    // private static final String JDBC_CONN_STRING_NEW_DB = "jdbc:derby:"
+    // + DB_NAME + ";create=true";
+    private static final String JDBC_CONN_STRING_EXISTING_DB =
+            "jdbc:derby://localhost:21527/pcc";
+    private static final String JDBC_CONN_STRING_NEW_DB =
+            "jdbc:derby://localhost:21527/pcc;create=true";
+
     private static final String PROCESS_ID = "${processId}";
     private static final String PARENT = "${parent}";
     private static final String STATE_BEING_ATTAINED = ":stateBeingAttained";
@@ -554,7 +559,8 @@ public class DefaultPersistence implements Persistence {
     @SuppressWarnings("unchecked")
     private void printDailyPlans(final String aText) {
         LOGGER.debug("{}: Daily plans (start)", aText);
-
+        LOGGER.debug("session: {}", session);
+        
         final Query debugQuery = session.createQuery("from DefaultDailyPlan");
 
         final List<DailyPlan> plans2 = (List<DailyPlan>) debugQuery.list();
@@ -1350,7 +1356,6 @@ public class DefaultPersistence implements Persistence {
         }
         return processes;
     }
-
 
     public Long getNextSchedulingObjectPriority(final SchedulingObject aParent) {
         Long maxPriority = 500L;
