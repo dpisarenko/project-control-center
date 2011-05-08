7e5e82a36a3103a760ba5e035907fe14e73bc4c9
Dmitri Pisarenko <dp@sw-dev.at>
2011-05-08 20:46:47 +0200

    Another attempt at bugfix: Old bookings, daily plans etc. are not deleted.

--- src/main/java/at/silverstrike/pcc/impl/persistence/DefaultPersistence.java
+++ src/main/java/at/silverstrike/pcc/impl/persistence/DefaultPersistence.java
@@ -364,10 +364,10 @@ public class DefaultPersistence implements Persistence {
         final Transaction tx = session.beginTransaction();
 
         try {
-            session.createQuery("delete DefaultDailyPlan").executeUpdate();
-            session.createQuery("delete DefaultDailySchedule").executeUpdate();
-            session.createQuery("delete DefaultDailyToDoList").executeUpdate();
-            session.createQuery("delete DefaultBooking").executeUpdate();
+            session.createQuery("delete from DefaultDailyPlan").executeUpdate();
+            session.createQuery("delete from DefaultDailySchedule").executeUpdate();
+            session.createQuery("delete from DefaultDailyToDoList").executeUpdate();
+            session.createQuery("delete from DefaultBooking").executeUpdate();
 
             final Date lastPlannedDay =
                     DateUtils.addDays(aNow, DAYS_TO_PLAN_AHEAD);
