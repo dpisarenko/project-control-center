7ce409c8ac8f2b38b7a530a99ff03843edd9ac32
Dmitri Pisarenko <dp@sw-dev.at>
2011-05-17 12:50:59 +0200

    Attempt at bugfix: Calendar panel is not displayed in the application.

--- src/main/java/at/silverstrike/pcc/impl/calendarpanel/DefaultCalendarPanel.java
+++ src/main/java/at/silverstrike/pcc/impl/calendarpanel/DefaultCalendarPanel.java
@@ -11,7 +11,12 @@
 
 package at.silverstrike.pcc.impl.calendarpanel;
 
+import java.util.Date;
+
+import org.apache.commons.lang.time.DateUtils;
+
 import com.google.inject.Injector;
+import com.vaadin.addon.calendar.ui.Calendar;
 import com.vaadin.ui.Panel;
 
 import at.silverstrike.pcc.api.calendarpanel.CalendarPanel;
@@ -20,23 +25,30 @@ import at.silverstrike.pcc.api.calendarpanel.CalendarPanel;
  * @author DP118M
  *
  */
-class DefaultCalendarPanel implements CalendarPanel {
-    private Injector injector;
+class DefaultCalendarPanel extends Panel implements CalendarPanel {
+    private static final int SEVEN_DAYS = 7;
+    private static final long serialVersionUID = 1L;
     
     @Override
     public Panel toPanel() {
-        // TODO Auto-generated method stub
-        return null;
+        return this;
     }
 
     @Override
     public void initGui() {
-        // TODO Auto-generated method stub
-
+        final Calendar calendar = new Calendar();
+        final Date now = new Date();
+        final Date inOneWeek = DateUtils.addDays(now, SEVEN_DAYS);
+        
+        calendar.setStartDate(now);
+        calendar.setEndDate(inOneWeek);
+        
+        calendar.setSizeFull();
+        
+        this.addComponent(calendar);
     }
     
     @Override
     public void setInjector(final Injector aInjector) {
-        this.injector = aInjector;
     }
 }
