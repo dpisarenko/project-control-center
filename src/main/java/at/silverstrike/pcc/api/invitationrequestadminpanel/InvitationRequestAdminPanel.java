b7c78d6cd7d5983348af99ca3381c43a1694e59c
Dmitri Pisarenko <dp@sw-dev.at>
2011-05-27 23:30:25 +0200

    Initial commit.

---
+++ src/main/java/at/silverstrike/pcc/api/invitationrequestadminpanel/InvitationRequestAdminPanel.java
@@ -0,0 +1,31 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010, 2011 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
+package at.silverstrike.pcc.api.invitationrequestadminpanel;
+
+import ru.altruix.commons.api.di.ModuleWithInjectableDependencies;
+import ru.altruix.commons.api.gui.ExternallyControlledGuiComponent;
+import ru.altruix.commons.api.vaadin.AbstractedPanel;
+import at.silverstrike.pcc.api.centraleditingpanelcontroller.CentralEditingPanelController;
+
+import com.vaadin.ui.Panel;
+
+/**
+ * @author DP118M
+ * 
+ */
+public interface InvitationRequestAdminPanel
+        extends
+        ModuleWithInjectableDependencies,
+        AbstractedPanel,
+        ExternallyControlledGuiComponent<InvitationRequestAdminPanelController, Panel> {
+
+}
