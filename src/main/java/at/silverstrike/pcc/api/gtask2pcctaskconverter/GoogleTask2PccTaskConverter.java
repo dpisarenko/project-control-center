6c42ad5f48e8fbb0a9c4c2f825409c0585ff8f25
Dmitri Pisarenko <dp@sw-dev.at>
2011-06-10 16:19:02 +0200

    Initial commit.

---
+++ src/main/java/at/silverstrike/pcc/api/gtask2pcctaskconverter/GoogleTask2PccTaskConverter.java
@@ -0,0 +1,22 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010, 2011 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
+package at.silverstrike.pcc.api.gtask2pcctaskconverter;
+
+import ru.altruix.commons.api.conventions.SingleActivityModule;
+
+/**
+ * @author DP118M
+ *
+ */
+public interface GoogleTask2PccTaskConverter extends SingleActivityModule {
+    
+}
