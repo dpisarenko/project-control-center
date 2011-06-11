d75b5c33b91a17121e1e43c12c7441d362f1e9b9
Dmitri Pisarenko <dp@sw-dev.at>
2011-06-11 20:51:35 +0200

    Implemented test cases in at.silverstrike.pcc.test.gtaskprioritycalculator.TestDefaultGoogleTasksPriorityCalculator.

--- src/test/java/at/silverstrike/pcc/test/gtaskprioritycalculator/TestDefaultGoogleTasksPriorityCalculator.java
+++ src/test/java/at/silverstrike/pcc/test/gtaskprioritycalculator/TestDefaultGoogleTasksPriorityCalculator.java
@@ -73,4 +73,40 @@ public class TestDefaultGoogleTasksPriorityCalculator {
             Assert.fail(exception.getMessage());
         }
     }
+    @Test
+    public void testTwoTasksReverse() {
+        final GoogleTasksPriorityCalculatorFactory factory =
+                new DefaultGoogleTasksPriorityCalculatorFactory();
+        final GoogleTasksPriorityCalculator objectUnderTest = factory.create();
+
+        final Map<String, com.google.api.services.tasks.v1.model.Task> inputData =
+                new HashMap<String, com.google.api.services.tasks.v1.model.Task>();
+
+        final com.google.api.services.tasks.v1.model.Task task1 = new Task();
+        task1.set("title", "Project 1");
+        task1.set("id", ID_1);
+        task1.set("position", "00000000001610612735");
+        
+
+        final com.google.api.services.tasks.v1.model.Task task2 = new Task();
+        task2.set("title", "Project 2");
+        task2.set("id", ID_2);
+        task2.set("position", "00000000001073741823");
+
+        inputData.put(ID_1, task1);
+        inputData.put(ID_2, task2);
+
+        try {
+            objectUnderTest.setTasks(inputData);
+            objectUnderTest.run();
+            final Map<String, Long> prioritiesByTaskIds =
+                    objectUnderTest.getPrioritiesByTaskIds();
+
+            Assert.assertEquals(new Long(900L), prioritiesByTaskIds.get(ID_1));
+            Assert.assertEquals(new Long(1000L), prioritiesByTaskIds.get(ID_2));
+        } catch (final PccException exception) {
+            LOGGER.error("", exception);
+            Assert.fail(exception.getMessage());
+        }
+    }
 }
