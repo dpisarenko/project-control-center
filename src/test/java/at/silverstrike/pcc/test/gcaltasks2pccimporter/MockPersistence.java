4d5ae5eb1590692a32d518eeadf5c19f8955d4dd
Dmitri Pisarenko <dp@sw-dev.at>
2011-06-22 12:23:37 +0200

    Attempt at bugfix: testDependenciesReadingPrefix(at.silverstrike.pcc.test.gcaltasks2pccimporter.TestDefaultGoogleCalendarTasks2PccImporter2)  Time elapsed: 0.665 sec  <<< ERROR! java.lang.NullPointerException 	at at.silverstrike.pcc.impl.gtask2pcctaskconverter.DefaultGoogleTask2PccTaskConverter.run(DefaultGoogleTask2PccTaskConverter.java:65) 	at at.silverstrike.pcc.impl.gcaltasks2pccimporter.DefaultGoogleCalendarTasks2PccImporter2.createPccTasks(DefaultGoogleCalendarTasks2PccImporter2.java:144) 	at at.silverstrike.pcc.impl.gcaltasks2pccimporter.DefaultGoogleCalendarTasks2PccImporter2.run(DefaultGoogleCalendarTasks2PccImporter2.java:73) 	at at.silverstrike.pcc.test.gcaltasks2pccimporter.TestDefaultGoogleCalendarTasks2PccImporter2.testDependencyCreation(TestDefaultGoogleCalendarTasks2PccImporter2.java:115) 	at at.silverstrike.pcc.test.gcaltasks2pccimporter.TestDefaultGoogleCalendarTasks2PccImporter2.testDependenciesReadingPrefix(TestDefaultGoogleCalendarTasks2PccImporter2.java:73) 	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)

--- src/test/java/at/silverstrike/pcc/test/gcaltasks2pccimporter/MockPersistence.java
+++ src/test/java/at/silverstrike/pcc/test/gcaltasks2pccimporter/MockPersistence.java
@@ -11,12 +11,28 @@
 
 package at.silverstrike.pcc.test.gcaltasks2pccimporter;
 
+import at.silverstrike.pcc.api.model.Task;
+import at.silverstrike.pcc.api.model.UserData;
 import at.silverstrike.pcc.test.mockpersistence.MockPersistenceAdapter;
+import at.silverstrike.pcc.test.model.MockObjectFactory;
 
 /**
  * @author DP118M
  *
  */
 final class MockPersistence extends MockPersistenceAdapter {
+    private static MockObjectFactory MOCK_OBJECT_FACTORY = new MockObjectFactory();
+    
+    @Override
+    public Task createSubTask(final String aProcessName, final Long aParentProcessId,
+            final UserData aUser) {
+        
+        final Task task = MOCK_OBJECT_FACTORY.createTask();
+        
+        task.setName(aProcessName);
+        task.setUserData(aUser);
+        
+        return task;
+    }
 
 }
