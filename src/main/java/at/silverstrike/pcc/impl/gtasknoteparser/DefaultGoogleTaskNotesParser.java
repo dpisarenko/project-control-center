1a9c46eefa281e3868f9803677ac1bee4bf62a88
Dmitri Pisarenko <dp@sw-dev.at>
2011-06-22 14:48:33 +0200

    Implemented method at.silverstrike.pcc.impl.gtasknoteparser.DefaultGoogleTaskNotesParser.parseDependenciesWithPrefix().

--- src/main/java/at/silverstrike/pcc/impl/gtasknoteparser/DefaultGoogleTaskNotesParser.java
+++ src/main/java/at/silverstrike/pcc/impl/gtasknoteparser/DefaultGoogleTaskNotesParser.java
@@ -11,6 +11,7 @@
 
 package at.silverstrike.pcc.impl.gtasknoteparser;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,14 +26,17 @@ import at.silverstrike.pcc.api.gtasknoteparser.GoogleTaskNotesParser;
  * 
  */
 class DefaultGoogleTaskNotesParser implements GoogleTaskNotesParser {
-    
+
+    private static final String PREFIX = "Depends on";
+    private static final String HASHTAG = "#";
     private String notes;
     private boolean effortSpecified;
-    private Pattern pattern;
+    private Pattern effortPattern;
     private double effortInHours;
 
     private boolean predecessorsSpecified;
     private List<String> predecessorLabels;
+    private Pattern predecessorPattern;
 
     public DefaultGoogleTaskNotesParser() {
         // This means N.YYh
@@ -40,22 +44,58 @@ class DefaultGoogleTaskNotesParser implements GoogleTaskNotesParser {
         // The fractional part is optional.
         final String effortRegex = ".*(\\d(\\.\\d{0,2}?)?)h.*";
 
-        pattern = Pattern.compile(effortRegex);
+        effortPattern = Pattern.compile(effortRegex);
+
+        final String dependenciesWithPrefixRegex =
+                "${prefix}\\:?+(.*)(,\\s.*)*\\s".replace("${prefix}", PREFIX);
+        this.predecessorPattern = Pattern.compile(dependenciesWithPrefixRegex);
     }
 
     @Override
     public void run() throws PccException {
         if (!StringUtils.isBlank(this.notes)) {
             parseEffort();
+            parseDependencies();
         } else {
             this.effortSpecified = false;
             this.predecessorsSpecified = false;
         }
     }
 
+    private void parseDependencies() {
+        if (this.notes.contains(PREFIX)) {
+            parseDependenciesWithPrefix();
+        } else if (this.notes.contains(HASHTAG)) {
+            parseDependenciesWithHashtags();
+        }
+    }
+
+    private void parseDependenciesWithHashtags() {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void parseDependenciesWithPrefix() {
+        final Matcher matcher =
+                this.predecessorPattern.matcher(this.notes.trim());
+        this.predecessorsSpecified = matcher.matches();
+
+        final int numberOfPredecessors = matcher.groupCount();
+
+        this.predecessorLabels = new LinkedList<String>();
+        for (int i = 0; i < numberOfPredecessors; i++) {
+            String text = StringUtils.trimToEmpty(matcher.group(i));
+
+            if (text.startsWith(",")) {
+                text = text.replace(",", "");
+            }
+
+            this.predecessorLabels.add(text);
+        }
+    }
 
     private void parseEffort() {
-        final Matcher matcher = this.pattern.matcher(this.notes.trim());
+        final Matcher matcher = this.effortPattern.matcher(this.notes.trim());
         this.effortSpecified = matcher.matches();
 
         if (this.effortSpecified) {
